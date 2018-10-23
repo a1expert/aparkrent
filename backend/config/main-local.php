ef3eb603d2318c22e044f6a5c9a87ee3f63d8c12
DmitryBormisov <dmb@goldcarrot.ru>
2018-10-23 19:10:32 +0500

    не должно быть в гите, add gitignore

--- backend/config/main-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'components' => [
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'ztdODwZ0uusy6Y-L5iroYX2iyeNz4oF1',
-        ],
-    ],
-];
