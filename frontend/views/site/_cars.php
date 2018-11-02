3c9495b75882d3463f50a97e4ee03969251dab90
DmitryBormisov <dmb@goldcarrot.ru>
2018-11-02 17:13:49 +0500

    fix

--- frontend/views/site/_cars.php
+++ frontend/views/site/_cars.php
@@ -23,9 +23,9 @@ use frontend\models\AutoModel;
                 <!-- <div class="car-desc"><?= $model->description ?></div> -->
                 <div class="nums-auto-buttons-wrap">
                     <div class="free-autos">
-                        <?php if($model->id = 3) : ?>
+                        <?php /*if($model->id == 3) : */?><!--
                             <div class="text">Доступен с 10 ноября</div>
-                        <?php endif; ?>
+                        --><?php /*endif; */?>
                         <?php if ($model->getFreeCars() != null) : ?>
                             <div class="text">Свободно автомобилей: <?= $model->getCountFree() ?></div>
                             <div class="auto-statuses">
